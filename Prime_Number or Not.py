8a415b8284f46d6f63d2bef64a85ac2462c37646
AkulSikand02 <83877359+AkulSikand02@users.noreply.github.com>
2021-06-14 17:08:39 +0530

    Update Prime_Number or Not.py

--- Prime_Number or Not.py
+++ Prime_Number or Not.py
@@ -3,6 +3,8 @@ print("Enter Number to check of prime or not")
 
 a = int(input("Enter a Number:"))
 Prime  = True
+if a <= 1 or a % 1 > 0:
+   Prime = False
 for b in range (2,a):
     if(a%b  == 0):
        Prime = False
